ef014ab3e6118c28360e2dbffb7f02848bcbd95f
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-07-05 15:00:38 +0530

    All about View-level permissions

--- library/posts/views.py
+++ library/posts/views.py
@@ -1,3 +1,4 @@
+from rest_framework import permissions
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Post
 from .serializers import PostSerializer
@@ -11,3 +12,6 @@ class PostList(ListCreateAPIView):
 class PostDetail(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    # permission_classes = [permissions.IsAdminUser]
+
+# NOTE: for view level permissions, we can use the permission_classes attribute.
