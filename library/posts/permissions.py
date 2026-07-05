60621fa0fd687e8e0524366e92d0af19d3ff0381
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-07-05 15:16:01 +0530

    All about Custom permissions

---
+++ library/posts/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+    # Authenticated users only can see list view
+        if request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request so we'll always
+        # allow GET, HEAD, or OPTIONS requests
+        if request.method in SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the author of a post
+        return obj.author == request.user
+
+# NOTE: for custom permissions, you will mostly need a dedicated permissions.py file. This is because you will need to override the BasePermission class to have your own logic in it, and set that instead as the permission class for your views. That definition of your inheriting permission class will sit in permissions.py.
+
+# has_permission() is called to check if the user has permission to access the view.
+# has_object_permission() is called to check what permissions the user has on a specific object.
--- library/posts/views.py
+++ library/posts/views.py
@@ -2,16 +2,18 @@ from rest_framework import permissions
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostList(ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-
+    permission_classes = [IsAuthorOrReadOnly]
+    
 class PostDetail(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     # permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAuthorOrReadOnly]
 
 # NOTE: for view level permissions, we can use the permission_classes attribute.
