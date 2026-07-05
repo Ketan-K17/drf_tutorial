4b53080f29fd419048101df95b95179c9626f53a
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2026-07-05 22:03:15 +0530

    Viewsets: Add user serialization and views to demonstrate why viewsets can be used

--- library/posts/serializers.py
+++ library/posts/serializers.py
@@ -1,5 +1,10 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+
 from .models import Post
+
+User = get_user_model()
+
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,3 +17,12 @@ class PostSerializer(serializers.ModelSerializer):
         )
         # NOTE: we've not added 'updated_at' field to the serializer.. that value should be decided by backend.
         model = Post
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+        )
--- library/posts/views.py
+++ library/posts/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from rest_framework import permissions
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
+
+User = get_user_model()
 
 
 class PostList(ListCreateAPIView):
@@ -15,5 +18,17 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
     # permission_classes = [permissions.IsAdminUser]
     permission_classes = [IsAuthorOrReadOnly]
+
+
+class UserList(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class UserDetail(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 # NOTE: for view level permissions, we can use the permission_classes attribute.
